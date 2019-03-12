3b1da696f5fd1ae1a83a23519ca99c9fbb441524
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-03-12 09:20:56 +0100

    Priority Queue

--- queues.js
+++ queues.js
@@ -35,3 +35,39 @@ q.print();
 //queue. if all elements have the same priority it behaves like a normal queue.
 
 
+function PriorityQueue(){
+	var collection = [];
+	this.printCollection = function(){
+		(console.log(collection));
+	};
+	this.enqueue = function(element){
+		if (this.isEmpty()) {
+			collection.push(element);
+		} else {
+			var added = false;
+			for(var i =0; i<collection.length; i++){
+				if (element[1] < collection[i][1]) {
+					collection.splice(i,0,element);
+					added = true;
+					break;
+				}
+			}
+			if (!added) {
+				collection.push(element);
+			}
+		};
+		this.dequeue = function(){
+			var value = collection.shift();
+			return value[0];
+		};
+		this.front = function(){
+			return collection[0];
+		};
+		this.size = function(){
+			return collection.length;
+		}
+		this.isEmpty = function(){
+			return (collection.length === 0);
+		};
+	}
+}
