2d4e3bf5a78f7bb9c31c0486d8ccff48beb2b755
sainttobs <sainttobs@gmail.com>
2019-03-14 14:41:53 +0100

    traversing a tree

--- bSearchTree.js
+++ bSearchTree.js
@@ -117,6 +117,78 @@ class BST {
 		}
 		this.root = removeNode(this.root, data);
 	}
+	isBalanced(){
+		return (this.findMinHeight() >= this.findMaxHeight() - 1)
+	}
+	findMinHeight(node = this.root){
+		if (node == null) {
+			return -1;
+		};
+		let left = this.findMinHeight(node.left);
+		let right = this.findMinHeight(node.right);
+		if (left < right) {
+			return left + 1;
+		} else {
+			return right + 1;
+		};
+	}
+	findMaxHeight(node = this.root) {
+		if (node == null) {
+			return -1;
+		};
+		let left = this.findMaxHeight(node.left);
+		let right = this.findMaxHeight(node.right);
+		if (left > right) {
+			return left + 1;
+		} else {
+			return right + 1;
+		};
+	}
+	inOrder() {
+		if (this.root == null) {
+			return null;
+		} else {
+			var result = new Array();
+			function traverseInOrder(node){
+				node.left && traverseInOrder(node.left);
+				result.push(node.data);
+				node.right && traverseInOrder(node.right);
+			}
+			traverseInOrder(this.root);
+			return result;
+		};
+	}
+	preOrder(){
+		if (this.root == null) {
+			return null;
+		} else {
+			var result = new Array()
+			function traversePreOrder(node){
+				result.push(node.data);
+				node.left && traversePreOrder(node.left);
+				node.right && traversePreOrder(node.right);
+			};
+			traversePreOrder(this.root);
+			return result;
+		};
+	}
+	postOrder(){
+		if (this.root == null) {
+			return null;
+		} else {
+			var result = new Array()
+			function traversePreOrder(node){
+				node.left && traversePostOrder(node.left);
+				node.right && traversePostOrder(node.right);
+				result.push(node.data);
+			};
+			traversePostOrder(this.root);
+			return result;
+		};
+	}
+	levelOrder(){
+		let result = []
+	}
 }
 
 
