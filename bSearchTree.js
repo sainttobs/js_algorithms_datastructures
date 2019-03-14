a49d61ecfb6e09bd55382b53b8c1ba41726b23d0
sainttobs <sainttobs@gmail.com>
2019-03-14 14:47:06 +0100

    height of a tree

--- bSearchTree.js
+++ bSearchTree.js
@@ -187,8 +187,25 @@ class BST {
 		};
 	}
 	levelOrder(){
-		let result = []
-	}
+		let result = [];
+		let Q = [];
+		if (this.root != null){
+			Q.push(this.root);
+			while(Q.length > 0){
+				let node = Q.shift();
+				result.push(node.data);
+				if (node.left != null) {
+					Q.push(node.left);
+				};
+				if (node.right != null) {
+					Q.push(node.right);
+				};
+			};
+			return result;
+		} else {
+			return null;
+		};
+	};
 }
 
 
